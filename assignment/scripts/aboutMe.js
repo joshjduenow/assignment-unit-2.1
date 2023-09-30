c5c0d44109fcaa1d577b036fc39da912860e63eb
Joshua Duenow <joshjduenow@gmail.com>
2023-09-30 07:37:54 -0500

    add lastName

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -2,7 +2,7 @@
 // 1 - Create a variable called `firstName` and assign it the value of your first name
 let firstName = 'Joshua';
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
-
+let lastName = 'Duenow';
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 //     (remember, you already have variables for this, can you use those?)
 //     Console log the value of `fullName`
